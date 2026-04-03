1ae84620a083a8ca43e8dd46cfbc995bd4ec4270
Chibueze Igbokwe <igbokwe.dev@gmail.com>
2026-04-03 10:08:38 +0100

    Create Activity.cs

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Threading;
+
+// Base class for all activities (Abstraction + Encapsulation)
+public abstract class Activity
+{
+    private string _name;
+    private string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine($"=== {_name} ===\n");
+        Console.WriteLine(_description);
+
+        Console.Write("\nEnter duration in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nPrepare to begin...");
+        ShowSpinner(3);
+    }
+
+    public void End()
+    {
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(2);
+
+        Console.WriteLine($"\nYou completed {_name} for {_duration} seconds.");
+        ShowSpinner(3);
+
+        Console.Clear();
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        DateTime end = DateTime.Now.AddSeconds(seconds);
+
+        int i = 0;
+        while (DateTime.Now < end)
+        {
+            Console.Write(spinner[i % spinner.Length]);
+            Thread.Sleep(200);
+            Console.Write("\b");
+            i++;
+        }
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+    public abstract void Run();
+}
