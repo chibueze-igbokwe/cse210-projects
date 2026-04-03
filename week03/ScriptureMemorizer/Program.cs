36c107f76d1804ddf2c6f73f78824214b3adc15a
Chibueze Igbokwe <igbokwe.dev@gmail.com>
2026-04-03 08:02:50 +0100

    Add Scripture classes and update Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // CREATIVITY: This program only hides words that are not already hidden
+
+        Reference reference = new Reference("John", 3, 16);
+
+        Scripture scripture = new Scripture(
+            reference,
+            "For God so loved the world that he gave his only begotten Son"
+        );
+
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,68 @@
+// week03/ScriptureMemorizer/Scripture.cs
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] splitWords = text.Split(" ");
+
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = _random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+
+            // Stop if all words are hidden
+            if (IsCompletelyHidden())
+            {
+                break;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
