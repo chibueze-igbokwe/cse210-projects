237cd9dd53d4e96c622bd32e422004e1471fabfd
Chibueze Igbokwe <igbokwe.dev@gmail.com>
2026-04-03 10:11:12 +0100

    Update Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("\nSelect an option: ");
+
+            string choice = Console.ReadLine();
+
+            Activity activity = null;
+
+            if (choice == "1")
+                activity = new BreathingActivity();
+            else if (choice == "2")
+                activity = new ReflectionActivity();
+            else if (choice == "3")
+                activity = new ListingActivity();
+            else if (choice == "4")
+                break;
+
+            if (activity != null)
+                activity.Run();
+
+            Console.Clear();
+        }
     }
 }
