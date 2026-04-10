c8763b3de17ae46d2909fd34ee7146ef312a3798
Chibueze Igbokwe <igbokwe.dev@gmail.com>
2026-04-10 20:55:50 +0100

    Create CyclingActivity.cs

---
+++ week07/ExerciseTracking/CyclingActivity.cs
@@ -0,0 +1,28 @@
+using System;
+
+// Cycling stores speed
+public class CyclingActivity : Activity
+{
+    private double _speed; // mph
+
+    public CyclingActivity(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * GetMinutes()) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+}
