637708b5768f875295c69e57f9430c55106a159a
Chibueze Igbokwe <igbokwe.dev@gmail.com>
2026-04-10 20:55:20 +0100

    Create RunningActivity.cs

---
+++ week07/ExerciseTracking/RunningActivity.cs
@@ -0,0 +1,28 @@
+using System;
+
+// Running stores distance directly
+public class RunningActivity : Activity
+{
+    private double _distance; // miles
+
+    public RunningActivity(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / _distance;
+    }
+}
